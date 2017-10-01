0df3aa4de71e6dc341fb6716315461aa07172d5f
igorgo <igorgo16@gmail.com>
2017-10-01 22:43:17 +0300

    _

--- src/controllers/conditions.js
+++ src/controllers/conditions.js
@@ -1,23 +1,23 @@
 'use strict'
 
-const m =require('../messages')
-const db =require('../db')
-const routine =require('./routine')
-const log =require('../logger')
-const _ =require('lodash')
+const m = require('../messages')
+const db = require('../db')
+const routine = require('./routine')
+const log = require('../logger')
+const _ = require('lodash')
 
 const conditions = module.exports
 
-async function getClaimConditions(socket) {
+async function getClaimConditions (socket) {
   if (socket.sessionID) {
     try {
       const res = await db.execute(socket.sessionID, 'select N01 RN, S01 SNAME, S02 EDITABLE from table(UDO_PACKAGE_NODEWEB_IFACE.GET_CONDITIONS_LIST)')
       socket.emit('claim_conditions_list', res.rows)
     } catch (e) {
-      routine.emitExecutionError(e,socket)
+      routine.emitExecutionError(e, socket)
     }
 
-  } else socket.emit('unauthorized', {message: m.MSG_DONT_AUTHORIZED})
+  } else socket.emit('unauthorized', { message: m.MSG_DONT_AUTHORIZED })
 }
 
 async function getClaimCondition (socket, pl) {
@@ -55,18 +55,98 @@ async function getClaimCondition (socket, pl) {
       const res = await db.execute(socket.sessionID, sql, params)
       socket.emit('claim_condition_got', res.outBinds)
     } catch (e) {
-      routine.emitExecutionError(e,socket)
+      routine.emitExecutionError(e, socket)
     }
+  } else socket.emit('unauthorized', { message: m.MSG_DONT_AUTHORIZED })
+}
 
-  } else socket.emit('unauthorized', {message: m.MSG_DONT_AUTHORIZED})
+async function saveClaimCondition (socket, {
+  rn,
+  name,
+  claimNumb,
+  claimVersion,
+  claimRelease,
+  claimBuild,
+  claimUnit,
+  claimApp,
+  imInitiator,
+  imExecutor,
+  claimContent
+}) {
+  if (!socket.sessionID) {
+    socket.emit('unauthorized', { message: m.MSG_DONT_AUTHORIZED })
+    return
+  }
+  const sql = `begin
+    UDO_PACKAGE_NODEWEB_IFACE.STORE_FILTER(
+      P_FILTER_RN     => :P_FILTER_RN,
+      P_FILTER_NAME   => :P_FILTER_NAME,
+      P_CLAIM_NUMB    => :P_CLAIM_NUMB,
+      P_CLAIM_VERS    => :P_CLAIM_VERS,
+      P_CLAIM_RELEASE => :P_CLAIM_RELEASE,
+      P_CLAIM_BUILD   => :P_CLAIM_BUILD,
+      P_CLAIM_UNIT    => :P_CLAIM_UNIT,
+      P_CLAIM_APP     => :P_CLAIM_APP,
+      P_CLAIM_IM_INIT => :P_CLAIM_IM_INIT,
+      P_CLAIM_IM_PERF => :P_CLAIM_IM_PERF,
+      P_CLAIM_CONTENT => :P_CLAIM_CONTENT,
+      P_OUT_RN        => :P_OUT_RN
+    );
+  end;`
+  const params = db.createParams()
+  params.add('P_FILTER_RN').dirIn().typeNumber().val(rn)
+  params.add('P_FILTER_NAME').dirIn().typeString().val(name)
+  params.add('P_CLAIM_NUMB').dirIn().typeString().val(claimNumb)
+  params.add('P_CLAIM_VERS').dirIn().typeString().val(claimVersion)
+  params.add('P_CLAIM_RELEASE').dirIn().typeString().val(claimRelease)
+  params.add('P_CLAIM_BUILD').dirIn().typeString().val(claimBuild)
+  params.add('P_CLAIM_UNIT').dirIn().typeString().val(claimUnit)
+  params.add('P_CLAIM_APP').dirIn().typeString().val(claimApp)
+  params.add('P_CLAIM_IM_INIT').dirIn().typeNumber().val(imInitiator)
+  params.add('P_CLAIM_IM_PERF').dirIn().typeNumber().val(imExecutor)
+  params.add('P_CLAIM_CONTENT').dirIn().typeString().val(claimContent)
+  params.add('P_OUT_RN').dirOut().typeNumber()
+  try {
+    const res = await db.execute(socket.sessionID, sql, params)
+    socket.emit('claim_condition_saved', res.outBinds)
+  } catch (e) {
+    routine.emitExecutionError(e, socket)
+  }
+}
+
+async function deleteClaimCondition (socket, { rn }) {
+  if (!socket.sessionID) {
+    socket.emit('unauthorized', { message: m.MSG_DONT_AUTHORIZED })
+    return
+  }
+  const sql = `begin
+    UDO_PACKAGE_NODEWEB_IFACE.DELETE_FILTER(
+      P_FILTER_RN => :P_FILTER_RN
+    );
+  end;`
+  const params = db.createParams()
+  params.add('P_FILTER_RN').dirIn().typeNumber().val(rn)
+  try {
+    await db.execute(socket.sessionID, sql, params)
+    socket.emit('claim_condition_deleted')
+  } catch (e) {
+    routine.emitExecutionError(e, socket)
+  }
 }
 
 conditions.init = socket => {
   socket.on('get_claim_conditions_list', () => {
-    getClaimConditions(socket)
+    void getClaimConditions(socket)
   })
   socket.on('get_claim_condition', (pl) => {
-    getClaimCondition(socket, pl)
+    void getClaimCondition(socket, pl)
   })
+  socket.on('save_claim_condition', (pl) => {
+    void saveClaimCondition(socket, pl)
+  })
+  socket.on('delete_claim_condition', (pl) => {
+    void deleteClaimCondition(socket, pl)
+  })
+
 }
 
